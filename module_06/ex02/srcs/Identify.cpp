004b922d2c3c564a2b03a79e53d83ebaacd3aaab
Alisa Arbenina <aarbenina@gmail.com>
2025-04-07 15:30:27 +0300

    module_06

--- module_06/ex02/srcs/Identify.cpp
+++ module_06/ex02/srcs/Identify.cpp
@@ -1,35 +1,33 @@
-#include <cstdlib>    // srand, rand
-#include <ctime>      // time
-#include <iostream>
+#include "Identify.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
-#include "Identify.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 Base* generate(void) {
-
     static bool seeded = false;
     if (!seeded) {
         std::srand(static_cast<unsigned int>(std::time(nullptr)));
         seeded = true;
     }
 
-    int r = std::rand() % 3; 
+    int r = std::rand() % 3;
     switch (r) {
-        case 0:
-            std::cout << "[generate()] Creating A" << std::endl;
-            return new A();
-        case 1:
-            std::cout << "[generate()] Creating B" << std::endl;
-            return new B();
-        default:
-            std::cout << "[generate()] Creating C" << std::endl;
-            return new C();
+    case 0:
+        std::cout << "[generate()] Creating A" << std::endl;
+        return new A();
+    case 1:
+        std::cout << "[generate()] Creating B" << std::endl;
+        return new B();
+    default:
+        std::cout << "[generate()] Creating C" << std::endl;
+        return new C();
     }
 }
 
 void identify(Base* p) {
-
     if (dynamic_cast<A*>(p) != nullptr)
         std::cout << "A" << std::endl;
     else if (dynamic_cast<B*>(p) != nullptr)
@@ -41,24 +39,23 @@ void identify(Base* p) {
 }
 
 void identify(Base& p) {
-
     try {
         (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
         return;
-    } catch (std::bad_cast &bc) {
+    } catch (std::bad_cast& bc) {
     }
     try {
         (void)dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
         return;
-    } catch (std::bad_cast &bc) {
+    } catch (std::bad_cast& bc) {
     }
     try {
         (void)dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
         return;
-    } catch (std::bad_cast &bc) {
+    } catch (std::bad_cast& bc) {
     }
 
     std::cout << "Unknown type" << std::endl;
